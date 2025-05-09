ca8abe064b2e5e5c4e3e144dfec1fc9b134411b4
seahpark247 <famousjedo@gmail.com>
2025-05-09 16:21:28 -0500

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Seah Park on 5/9/25.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EdutainmentTests/EdutainmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentTests.swift
+//  EdutainmentTests
+//
+//  Created by Seah Park on 5/9/25.
+//
+
+import Testing
+@testable import Edutainment
+
+struct EdutainmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
