9f637dce19e11b8da023ae4e3f4d26aa8bb7f8f5
seahpark247 <famousjedo@gmail.com>
2025-05-09 16:52:57 -0500

    feat: main ui

--- Edutainment/ContentView.swift
+++ Edutainment/ContentView.swift
@@ -7,16 +7,46 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+struct ButtonStyle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle.bold())
+            .frame(maxWidth: .infinity)
+            .padding(.vertical, 4)
+            .background(.blue.opacity(0.2))
+            .cornerRadius(10)
     }
+}
+
+extension View {
+    func buttonStyle() -> some View {
+        self.modifier(ButtonStyle())
+    }
+}
+
+struct ContentView: View {
+    @State private var selectedTab: Int? = 0
+    
+    var body: some View {
+        NavigationStack {
+            Spacer()
+            Text("Which multiplication table would you like to learn?").font(.largeTitle)
+            
+            VStack {
+                ForEach(2..<13){ i in
+                    Button("\(i)") {
+                        selectTab(i)
+                    }.buttonStyle()
+                }
+            }.padding()
+        }
+    }
+    
+    func selectTab(_ index: Int) {
+        selectedTab = index
+    }
+    
+    
 }
 
 #Preview {
